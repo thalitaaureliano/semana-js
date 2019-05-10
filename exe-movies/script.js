6f5e36fbbbb5903b47d6b9b4eb2c819df0d00968
thalitaaureliano <thalitaaurel@gmail.com>
2019-05-10 20:15:35 -0300

    usandoFetch

--- exe-movies/script.js
+++ exe-movies/script.js
@@ -1,11 +1,12 @@
 const button = document.querySelector(".btn-search-movie");
+const texto = document.getElementById("movie");
+const containerMovies = document.querySelector('.movies');
+
+console.log(texto)
 
 button.addEventListener('click', (evento) =>{
     evento.preventDefault();
 
-    const filmes = document.querySelector(".form");
-    const texto = document.getElementById("movie");
-    console.log(texto)
 
     fetch(`http://www.omdbapi.com/?s=${texto.value}&apikey=c4715b31`)
     .then((response) => {
@@ -13,19 +14,38 @@ button.addEventListener('click', (evento) =>{
     })
     .then((data) => {
         console.log(data)
-        filmes.innerHTML="";
 
-        data.search.forEach(filme => {
+        data.Search.forEach(filme => {
             console.log(filme)
 
             const box = document.createElement('div');
             box.setAttribute('class', 'box');
-            filmes.appendChild(box);
+            containerMovies.appendChild(box);
+
             
             const imagem = document.createElement('img');
+            imagem.setAttribute('src', filme.Poster)
             box.appendChild(imagem);
-            imagem.src = filmes.poster;
+
+            const boxDivider = document.createElement('div');
+            boxDivider.setAttribute('class', 'box-divider');
+            box.appendChild(boxDivider);
+
+            const p = document.createElement('p');
+            boxDivider.appendChild(p);
+
+            const spanTitulo = document.createElement('span');
+            spanTitulo.innerHTML = `${filme.Title} (${filme.Year})`;
+            p.appendChild(spanTitulo);
+
+            const pType = document.createElement('p');
+            pType.innerHTML = filme.Type;
+            p.appendChild(pType);
             
+            button.addEventListener('click', () => {
+                box.remove();
+            })
+
         }) 
     })
     .catch((erro) => {
